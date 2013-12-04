36f1719bbc7a74e01877cd59ef9e2ec5cf42bf3b
Kevin Seiden <alshain01@suddenlink.net>
2013-12-04 08:12:34 -0600

    Remove eclipse

---
+++ src/main/java/io/github/alshain01/NoMoreZombies/Main.java
@@ -0,0 +1,24 @@
+package io.github.alshain01.NoMoreZombies;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		this.getServer().getPluginManager().registerEvents(new SpawnListener(), this);
+	}
+	
+	private class SpawnListener implements Listener {
+		@EventHandler(priority = EventPriority.HIGHEST)
+		private void onCreatureSpawn(CreatureSpawnEvent e) {
+			if(e.getSpawnReason() != SpawnReason.REINFORCEMENTS) { return; }
+			e.setCancelled(true);
+		}
+	}
+}
